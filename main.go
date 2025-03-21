c303e95d5ffe3ad43e7a6414442279d074de673e
Dhanar Adi Dewandaru <dhanar@accelbyte.net>
2025-03-21 10:11:52 +0700

    fix: handle sigterm properly

--- main.go
+++ main.go
@@ -16,6 +16,7 @@ import (
 	"os/signal"
 	"runtime"
 	"strings"
+	"syscall"
 	"time"
 
 	"session-dsm-grpc-plugin/pkg/client/awsgamelift"
@@ -255,6 +256,8 @@ func main() {
 	logrus.Infof("gRPC server started")
 	logrus.Infof("app server started")
 
-	ctx, _ = signal.NotifyContext(ctx, os.Interrupt)
+	ctx, stop := signal.NotifyContext(ctx, os.Interrupt, syscall.SIGTERM)
+	defer stop()
 	<-ctx.Done()
+	logrus.Infof("signal received")
 }
